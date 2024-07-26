8dd71f028e21db491d10d8e4158b8e3502e487a5
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-07-26 20:09:16 -0300

    feat: add license plate and car photo fields

--- cars/models.py
+++ cars/models.py
@@ -16,6 +16,8 @@ class Car(models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField()
+    plate = models.CharField(max_length=10, blank=True, null=True)
+    photo = models.ImageField(upload_to="cars/", blank=True, null=True)
 
     def __str__(self) -> str:
         return self.model
