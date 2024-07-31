535cb17b96213ddade99b27dbf9b0164c09fc4d9
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-07-31 15:26:26 -0300

    feat: Create view car_view to list cars and templates html

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+def cars_view(request):
+    cars = Car.objects.all()
+    req = request.GET.get("search")
+
+    if req:
+        cars = cars.filter(model__icontains=req)
+
+    return render(request, "cars.html", {"cars": cars})
