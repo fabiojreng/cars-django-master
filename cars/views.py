47832f5d0e3a7cb0eeefcd7987c699a0364e9d44
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-08-02 12:41:53 -0300

    feat: Create view for user  to add a car

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from cars.models import Car
+from cars.forms import CarModelForm
 
 
 def cars_view(request):
@@ -10,3 +11,14 @@ def cars_view(request):
         cars = cars.filter(model__icontains=req)
 
     return render(request, "cars.html", {"cars": cars})
+
+
+def new_car_view(request):
+    if request.method == "POST":
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect("cars_list")
+    else:
+        new_car_form = CarModelForm()
+    return render(request, "new_car.html", {"new_car_form": new_car_form})
