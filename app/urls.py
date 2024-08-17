c257e5e3a8fbb699dd24ecea4a4d08d7806c585a
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-08-17 12:24:36 -0300

    feat: Add car delete url

--- app/urls.py
+++ app/urls.py
@@ -2,15 +2,22 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsListView, NewCarView, CarDetailView, CarUpdateView
+from cars.views import (
+    CarsListView,
+    NewCarView,
+    CarDetailView,
+    CarUpdateView,
+    CarDeleteView,
+)
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("cars/", CarsListView.as_view(), name="cars_list"),
     path("new_car/", NewCarView.as_view(), name="new_car"),
-    path("car/<int:pk>/", CarDetailView.as_view(), name="car_datail"),
+    path("car/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
     path("car/<int:pk>/update/", CarUpdateView.as_view(), name="car_update"),
+    path("car/<int:pk>/delete/", CarDeleteView.as_view(), name="car_delete"),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
