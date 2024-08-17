b71c51ea842b04ee7250dae5d755b27309cf9fd4
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-08-17 11:24:16 -0300

    feat: Create detail view and edit view of the car

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 
 class CarsListView(ListView):
@@ -22,4 +22,16 @@ class NewCarView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = "new_car.html"
+    success_url = "/cars/"
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "car_detail.html"
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = "car_update.html"
     success_url = "/cars/"
